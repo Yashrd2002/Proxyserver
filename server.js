8b6308f0c8dd085661f0da8f475867354e3de4ef
Yashrd2002 <94217776+Yashrd2002@users.noreply.github.com>
2023-09-28 15:00:39 +0530

    first commitjkkkssjssd

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const app = express();
-
-const targetUrl = 'http://localhost:5173'; // Replace with your VR streaming server URL
-
-const proxyMiddleware = createProxyMiddleware({
-  target: targetUrl,
-  changeOrigin: true,
-});
-
-app.use('/vr-streaming', proxyMiddleware);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Proxy server is running on port ${PORT}`);
-});
+var http = require('http');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+const PORT = 3000
+http.createServer(function(req, res) {
+    proxy.web(req, res, { target: 'http://localhost:5173' });
+}).listen(PORT);
