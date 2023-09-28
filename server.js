7250fe95f826af82040252b50440806b4b0bac04
Yashrd2002 <94217776+Yashrd2002@users.noreply.github.com>
2023-09-28 18:24:30 +0530

    firstssl

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Origin, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
